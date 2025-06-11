4b68bdbd24f2ef235e2470908d371bd73e8a6bd6
DrumRK <icanfeelmyart@gmail.com>
2025-06-11 10:55:22 +0200

    Fix: aggiorna nomi squadre in giocatori.js

--- backend/data/giocatori.js
+++ backend/data/giocatori.js
@@ -1,10 +1,34 @@
 export const giocatori = [
-  { name: "ENZO", teams: ["Paris Saint Germain", "Borussia Dortmund", "Wydad AC", "Urawa"] },
-  { name: "ROB&CLA", teams: ["Palmeiras", "Inter", "Boca Juniors", "Pachuca"] },
-  { name: "MATTO DI LUSCIANO", teams: ["Manchester City", "Red Bull Salzburg", "Los Angeles FC", "Auckland City"] },
-  { name: "ZIO ALDO", teams: ["Fluminense", "Chelsea", "Monterrey", "Inter Miami"] },
-  { name: "DANI & CIRO", teams: ["Flamengo", "Benfica", "Al-Hilal Saudi FC", "Al Ain"] },
-  { name: "MARIO", teams: ["River Plate", "FC Porto", "Botafogo", "Mamelodi Sundowns"] },
-  { name: "UMBERTO", teams: ["Bayern München", "Atletico Madrid", "Ulsan Hyundai FC", "ES Tunis"] },
-  { name: "BENNY", teams: ["Real Madrid", "Juventus", "Al Ahly", "Seattle Sounders"] }
+  {
+    name: "ENZO",
+    teams: ["Paris Saint Germain", "Borussia Dortmund", "Wydad AC", "Urawa Red Diamonds"]
+  },
+  {
+    name: "ROB&CLA",
+    teams: ["SE Palmeiras", "FC Internazionale Milano", "CA Boca Juniors", "CF Pachuca"]
+  },
+  {
+    name: "MATTO DI LUSCIANO",
+    teams: ["Manchester City", "FC Salzburg", "Los Angeles FC", "Auckland City"]
+  },
+  {
+    name: "ZIO ALDO",
+    teams: ["Fluminense", "Chelsea", "CF Monterrey", "Inter Miami CF"]
+  },
+  {
+    name: "DANI & CIRO",
+    teams: ["CR Flamengo", "SL Benfica", "Al-Hilal Saudi FC", "Al Ain"]
+  },
+  {
+    name: "MARIO",
+    teams: ["River Plate", "FC Porto", "Botafogo", "Mamelodi Sundowns"]
+  },
+  {
+    name: "UMBERTO",
+    teams: ["FC Bayern München", "Atletico Madrid", "Ulsan Hyundai", "ES Tunis"]
+  },
+  {
+    name: "BENNY",
+    teams: ["Real Madrid", "Juventus", "Al Ahly", "Seattle Sounders"]
+  }
 ];
